7d6356018fb1840f73c57cbee147256f54cd7def
SunJames568 <sunjames568@gmail.com>
2022-09-27 15:06:23 -0400

    Initial Commit

---
+++ Project1/Models/Names.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Project1.Models
+{
+    public partial class Names
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
---
+++ Project1/Models/testmvcContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Project1.Models
+{
+    public partial class testmvcContext : DbContext
+    {
+        public testmvcContext()
+        {
+        }
+
+        public testmvcContext(DbContextOptions<testmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Names> Names { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress01;Database=test(mvc);Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Names>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Fname)
+                    .HasColumnName("fname")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Lname)
+                    .HasColumnName("lname")
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
